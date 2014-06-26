3305da749e092cc962814e007905635c3470e51f
Administrator <Administrator@lee-PC.local>
2014-06-26 18:37:43 +0900

    Problem_12

--- Euler/src/Problem_11_20/Problem_12.scala
+++ Euler/src/Problem_11_20/Problem_12.scala
@@ -2,24 +2,22 @@ package Problem_11_20
 
 object Problem_12 {
     def main(args: Array[String]): Unit = {
-        var count = 0
-        var triangularNum = 0
-        var k = 1
-        while (count < 5) {
-            count = 0
+        var triangularNum = 1
+        var k = 2
+        while (getCount(triangularNum) < 500) {
             triangularNum += k
             k += 1
-            var array = new Array[Int](triangularNum)
-            for (i <- 2 until array.length; if (array(i) != -1)) {
-                for (j <- i + i until array.length by i) {
-                    array(j) = -1
-                }
-            }
-            
-            for (value <- array; if (value != -1)) {
-                count += 1
-            }
         }
         println(triangularNum)
+    }
+
+    def getCount(size: Int): Int = {
+        var sqrt = math.sqrt(size).toInt
+        var count = 2
+        for (i <- 2 to sqrt; if (size % i == 0)) {
+            count += 2
+        }
+        println("num : " + size + " sqrt : " + math.sqrt(size).toInt + " ---- " + count)
+        return count
     }
 }
